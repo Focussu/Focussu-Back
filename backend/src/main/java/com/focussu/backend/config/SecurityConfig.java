8ffe2c09b1278b7742b8ab7ac67b18f93ad59284
oxdjww <oxdjww@gmail.com>
2025-04-15 18:50:03 +0900

    [FEAT] CORS 설정

--- backend/src/main/java/com/focussu/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/focussu/backend/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -36,6 +38,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers(DOCS_WHITELIST).permitAll()
