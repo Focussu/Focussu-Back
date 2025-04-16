1eb3fb24e94a336205ff1e30adafbceb92a922fb
oxdjww <oxdjww@gmail.com>
2025-04-16 12:25:43 +0900

    [FEAT] Swagger에 JWT Security 설정 적용

--- backend/src/main/java/com/focussu/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/focussu/backend/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,9 +24,21 @@ public class SwaggerConfig {
                 .url("https://focussu-api.life")
                 .description("개발 서버");
 
+        // Bearer 토큰을 위한 스키마 설정
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // 보안 요구사항 추가 (모든 API 엔드포인트에 적용되는 경우)
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
         return new OpenAPI()
                 .servers(List.of(localServer, prodServer))
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("BearerAuth", bearerScheme))
+                .addSecurityItem(securityRequirement)
                 .info(apiInfo());
     }
 
