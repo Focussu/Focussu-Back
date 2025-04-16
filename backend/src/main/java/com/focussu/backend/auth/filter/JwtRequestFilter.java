ef0e91616e11e117b72502f4908e2765fe1d4c87
oxdjww <oxdjww@gmail.com>
2025-04-16 12:23:17 +0900

    [FEAT] JwtRequestFilter에 화이트리스트 및 Redis 토큰 검증 추가

--- backend/src/main/java/com/focussu/backend/auth/filter/JwtRequestFilter.java
+++ backend/src/main/java/com/focussu/backend/auth/filter/JwtRequestFilter.java
@@ -1,7 +1,12 @@
 package com.focussu.backend.auth.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.focussu.backend.auth.exception.AuthException;
 import com.focussu.backend.auth.service.CustomUserDetailsService;
+import com.focussu.backend.auth.service.TokenService;
 import com.focussu.backend.auth.util.JwtTokenUtil;
+import com.focussu.backend.common.constant.WhiteList;
+import com.focussu.backend.common.exception.ErrorCode;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,10 +17,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+
+import static com.focussu.backend.common.constant.WhiteList.isWhitelisted;
 
 @Component
 @RequiredArgsConstructor
@@ -23,27 +32,40 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
     private final CustomUserDetailsService userDetailsService;
     private final JwtTokenUtil jwtTokenUtil;
+    private final TokenService tokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain chain) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
+        String requestUri = request.getRequestURI();
 
+        // whitelist에 해당하면 토큰 검사를 건너뛰도록 함
+        if (isWhitelisted(requestUri)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        final String authHeader = request.getHeader("Authorization");
         String username = null;
         String jwt = null;
 
-        // "Bearer {token}" 형식 검증
-        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
-            jwt = authHeader.substring(7);
-            try {
-                username = jwtTokenUtil.getUsernameFromToken(jwt);
-            } catch (Exception e) {
-                logger.error("JWT 토큰 파싱 오류: {} " + e.getMessage());
-            }
+        if (!StringUtils.hasText(authHeader)) {
+            // 토큰 미제공 시 예외 발생 (여기서 JwtExceptionFilter가 처리)
+            throw new AuthException(ErrorCode.AUTH_TOKEN_MISSING);
+        }
+        if (!authHeader.startsWith("Bearer ")) {
+            throw new AuthException(ErrorCode.AUTH_TOKEN_MALFORMED);
+        }
+        jwt = authHeader.substring(7);
+
+        // 파싱하며 토큰 유효성 검증 (예: 만료, 서명 오류 등)
+        username = jwtTokenUtil.getUsernameFromToken(jwt);
+
+        // 로그아웃 토큰
+        if (!tokenService.isTokenRevoked(jwt)) {
+            throw new AuthException(ErrorCode.AUTH_TOKEN_NOT_FOUND);
         }
 
-        // username이 존재하고 아직 인증 정보가 SecurityContext에 없다면
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             var userDetails = userDetailsService.loadUserByUsername(username);
             if (jwtTokenUtil.validateToken(jwt, userDetails)) {
