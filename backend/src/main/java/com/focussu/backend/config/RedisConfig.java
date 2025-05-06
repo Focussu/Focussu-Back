520b1f6a7e18281b5845d11b27fa8f7420dbd8e5
oxdjww <oxdjww@gmail.com>
2025-05-06 14:39:28 +0900

    [CHORE] 로컬 Redis 배포환경 ELC 연동을 추가한다.

--- backend/src/main/java/com/focussu/backend/config/RedisConfig.java
+++ backend/src/main/java/com/focussu/backend/config/RedisConfig.java
@@ -1,25 +1,36 @@
 package com.focussu.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration cfg = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(cfg);
+    }
 
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
-
-        template.afterPropertiesSet();
-        return template;
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory f) {
+        RedisTemplate<String, String> t = new RedisTemplate<>();
+        t.setConnectionFactory(f);
+        t.setKeySerializer(new StringRedisSerializer());
+        t.setValueSerializer(new StringRedisSerializer());
+        t.setHashKeySerializer(new StringRedisSerializer());
+        t.setHashValueSerializer(new StringRedisSerializer());
+        t.afterPropertiesSet();
+        return t;
     }
 }
